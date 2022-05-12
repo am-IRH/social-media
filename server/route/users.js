5107377a95981a9d7b4e26cdd1305914e7cc3819
AM-IRH <norouzzadeh.dev@gmail.com>
2022-05-13 03:19:06 +0430

    follow and unfollow api

--- server/route/users.js
+++ server/route/users.js
@@ -68,4 +68,56 @@ router.get("/:id", async (req, res) => {
     }
 })
 
+// follow a user
+router.put("/:id/follow", async (req, res) => {
+    if(req.body.userId !== req.params.id) {
+        try {
+            const user = await User.findById(req.params.id);
+            const currentUser = await User.findById(req.body.userId);
+
+            if(!user.followers.includes(req.body.userId)) {
+                await user.updateOne({$push: {followers: req.body.userId}});
+                await currentUser.updateOne({$push: {following: req.body.userId}});
+                return res.status(200).json({status: "success", success: true, text: "user has been followed"});
+            } else {
+                return res.status(403).json("you already follow this user");
+            }
+        } catch(err) {
+            return res.status(500).json(err);
+        }
+    } else {
+        return res.status(403).json({
+            status: "Forbidden",
+            success: false,
+            err: "You can't follow yourself"
+        });
+    }
+});
+
+// unfollow
+router.delete("/:id/unfollow", async (req, res) => {
+    if(req.body.userId !== req.params.id) {
+        try {
+            const user = await User.findById(req.params.id);
+            const currentUser = await User.findById(req.body.userId);
+
+            if(user.followers.includes(req.body.userId)) {
+                await user.updateOne({$pull: {followers: req.body.userId}});
+                await currentUser.updateOne({$pull: {following: req.body.userId}});
+                return res.status(200).json({status: "success", success: true, text: "user has been unfollowed"});
+            } else {
+                return res.status(403).json("you not unfollow this user");
+            }
+        } catch(err) {
+            return res.status(500).json(err);
+        }
+    } else {
+        return res.status(403).json({
+            status: "Forbidden", 
+            success: false,
+            err: "You can't unfollow yourself"
+        });
+    }
+});
+
 module.exports = router;
